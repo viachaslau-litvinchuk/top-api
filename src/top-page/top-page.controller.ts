828ff2a662eb820ec56ed4dbedf515ff97c4e21c
viachaslau-litvinchuk <viacaslau@gmail.com>
2022-11-17 11:22:19 +0400

    29

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
-import { findProductDto } from 'src/product/dto/find-product.dto';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model/top-page.model';
 
